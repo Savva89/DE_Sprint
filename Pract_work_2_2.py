2950ba67691875fe09caae59a633e9af0c3aeca5
Savva89 <a.v.borgardt@gmail.com>
2022-10-12 23:41:26 +0700

    Практическая работа 2

--- Pract_work_2_2.py
+++ Pract_work_2_2.py
@@ -1,9 +1,12 @@
 palindrom_string = input()
+
 palindrom_without_space = palindrom_string.replace(' ','')
 palindrom_lenght = len(palindrom_without_space) 
 palindrom = True
+
 for i in range(0,palindrom_lenght // 2):
     if palindrom_without_space[i] != palindrom_without_space[-(i+1)]:
        palindrom = False
        break 
+    
 print(palindrom) 
